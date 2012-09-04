af8c08f50f955b1255df7e79c906e793c3b23da5
Jake Hickenlooper <jake@weboftomorrow.com>
2012-09-04 23:38:18 +0000

    vb 0.0.1
    Works well enough to start using.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 name = "scissors"
-version = "0.0.0"
+version = "0.0.1"
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
